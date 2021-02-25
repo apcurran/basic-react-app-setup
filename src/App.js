c380bf427e027b7d0b54ffe6fc7de81e930bded3
Alex Curran <alexcurran.va@gmail.com>
2021-02-25 10:39:43 -0600

    Added example CSS file, also changed to separate dev and production webpack config files for clarity

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 
+import "./App.css";
+
 function App() {
     return (
         <div>
-            <h1>Hello React!</h1>
+            <h1>Hello there React!</h1>
         </div>
     );
 }
---
+++ webpack.prod.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: "production",
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "bundle.js",
+    },
+    resolve: {
+        modules: [path.join(__dirname, "src"), "node_modules"],
+        alias: {
+            react: path.join(__dirname, "node_modules", "react"),
+        },
+        symlinks: false,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                include: path.resolve(__dirname, "src"),
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader",
+                    },
+                    {
+                        loader: "css-loader",
+                    },
+                ]
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: "./src/index.html" }),
+    ],
+};
